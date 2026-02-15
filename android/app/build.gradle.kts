80dfcccf8b78b7033c910bbd51ef42fda84f2e02
Retired64 <159728675+retired64@users.noreply.github.com>
2026-02-14 21:12:53 -0800

    Specify NDK version 28.2.13676358 for F-Droid build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.impostormx.org"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
